825013bdde5a372a034f625cff337bfbd76968ea
Leonardo130709 <78939351+Leonardo130709@users.noreply.github.com>
2022-09-21 15:50:03 +0300

    dictionaries are transmitted key be key now to prevent pickle+socket memory issues.

--- ur_env/remote.py
+++ ur_env/remote.py
@@ -1,14 +1,16 @@
-from typing import Optional
+from typing import Tuple, Optional, Sequence, Dict, Any, Mapping, Union
 import abc
 import time
 import socket
 import pickle
 import logging
+from collections import OrderedDict
 
-from ur_env.base import Environment
+from ur_env import base
 
+Address = Tuple[str, int]
 DEFAULT_TIMEOUT = 20.
-PKG_SIZE = 1 << 32
+PKG_SIZE = 1 << 16
 
 _log = logging.getLogger(__name__)
 
@@ -26,7 +28,7 @@ def _assert_valid_size(data):
     # should be replaced with chunking.
     size = len(data)
     if size >= PKG_SIZE:
-        msg = f"Package size is close to max limit: {size} / {PKG_SIZE}."
+        msg = f"Package size is larger than maxsize: {size} / {PKG_SIZE}."
         _log.error(msg)
         raise ConnectionError(msg)
     return True
@@ -34,30 +36,52 @@ def _assert_valid_size(data):
 
 class RemoteBase(abc.ABC):
     """Unsafe and inefficient data transmission via pickle."""
-    def __init__(self, host: Optional[str] = None, port: Optional[int] = None):
+    def __init__(self, address: Optional[Address]):
         self._sock = None
-        if host and port:
-            self.connect(host, port)
+        if address:
+            self.connect(address)
 
     @abc.abstractmethod
-    def connect(self, host: str, port: int):
+    def connect(self, address: Address):
         """Establish connection."""
 
-    def _receive(self):
+    @abc.abstractmethod
+    def _negotiate_protocol(self):
+        """Declare how nested objects will be transferred."""
+
+    def _receive(self) -> Any:
         data = self._sock.recv(PKG_SIZE)
         _assert_valid_size(data)
         return pickle.loads(data)
 
-    def _send(self, data):
+    def _send(self, data: Any):
         data = pickle.dumps(data)
         _assert_valid_size(data)
         return self._sock.sendall(data)
+
+    def _send_dict(self, keys: Sequence[str], data: Dict[str, Any]):
+        """
+        A nested structure as a whole can be larger than the PKG_SIZE,
+        so it will be sent item by item.
+        dm-tree would come in handy here but for now only dicts are considered.
+        """
+        for key in keys:
+            self._send(data[key])
+
+    def _recv_dict(self, keys: Sequence[str]) -> Dict[str, Any]:
+        data = OrderedDict()
+        for key in keys:
+            value = self._receive()
+            if isinstance(value, Exception):
+                raise value
+            data[key] = value
+        return data
 
 
 class RemoteEnvClient(RemoteBase):
     """Client side of a remote robot env."""
 
-    def connect(self, host: str, port: int):
+    def connect(self, address: Address):
         if self._sock:
             return
 
@@ -66,11 +90,15 @@ class RemoteEnvClient(RemoteBase):
             self._sock.settimeout(DEFAULT_TIMEOUT)
             # self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             # self._sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            self._sock.connect((host, port))
+            self._sock.connect(address)
+            self._negotiate_protocol()
             _log.info("Connected")
         except (socket.timeout, socket.error):
             self._sock = None
             raise
+
+    def _negotiate_protocol(self):
+        self.__act_space_keys, self.__obs_space_keys = self._receive()
 
     def ping(self):
         start = time.time()
@@ -81,14 +109,17 @@ class RemoteEnvClient(RemoteBase):
         _log.info(msg)
         print(msg)
 
-    def step(self, action):
+    def step(self, action: Union[base.NDArray, base.NestedNDArray]):
         self._send(Command.STEP)
-        self._send(action)
-        return self._receive()
+        if self.__act_space_keys:
+            self._send_dict(self.__act_space_keys, action)
+        else:
+            self._send(action)
+        return self._recv_timestep()
 
     def reset(self):
         self._send(Command.RESET)
-        return self._receive()
+        return self._recv_timestep()
 
     def close(self):
         self._send(Command.CLOSE)
@@ -96,16 +127,23 @@ class RemoteEnvClient(RemoteBase):
         self._sock.shutdown(socket.SHUT_RDWR)
         self._sock.close()
         return resp
+    
+    def _recv_timestep(self):
+        obs = self._recv_dict(self.__obs_space_keys)
+        timestep = self._receive()
+        return timestep._replace(observation=obs)
 
     @property
     def action_space(self):
         self._send(Command.ACT_SPACE)
+        if self.__act_space_keys:
+            return self._recv_dict(self.__act_space_keys)
         return self._receive()
 
     @property
     def observation_space(self):
         self._send(Command.OBS_SPACE)
-        return self._receive()
+        return self._recv_dict(self.__obs_space_keys)
 
     @property
     def scene(self):
@@ -119,23 +157,33 @@ class RemoteEnvClient(RemoteBase):
 class RemoteEnvServer(RemoteBase):
     """Server side hosting a robot."""
 
-    def __init__(self, env: Environment, host: Optional[str] = None, port: Optional[int] = None):
+    def __init__(self, env: base.Environment, address: Address):
         self._env = env
-        super().__init__(host, port)
+        super().__init__(address)
 
-    def connect(self, host: str, port: int):
+    def connect(self, address: Address):
         if self._sock:
             return
 
         try:
             sock = socket.socket()
-            sock.bind((host, port))
-            sock.listen(1)
+            sock.bind(address)
+            sock.listen()
             self._sock, add = sock.accept()
+            self._negotiate_protocol()
             _log.info(f"Connection established: {add}.")
         except (socket.timeout, socket.error):
             self._sock = None
             raise
+
+    def _negotiate_protocol(self):
+        act_space = self._env.action_space
+        if isinstance(act_space, Mapping):
+            self.__act_space_keys = tuple(act_space.keys())
+        else:
+            self.__act_space_keys = None
+        self.__obs_space_keys = tuple(self._env.observation_space.keys())
+        self._send((self.__act_space_keys, self.__obs_space_keys))
 
     def run(self):
         cmd = None
@@ -149,28 +197,52 @@ class RemoteEnvServer(RemoteBase):
 
     def _on_receive(self, cmd: int):
         if cmd == Command.RESET:
-            resp = self.reset()
+            self.reset()
         elif cmd == Command.STEP:
-            action = self._receive()
-            resp = self.step(action)
+            self.step()
         elif cmd == Command.ACT_SPACE:
-            resp = self.action_space
+            self.action_space()
         elif cmd == Command.OBS_SPACE:
-            resp = self.observation_space
+            self.observation_space()
         elif cmd == Command.PING:
-            resp = "PING!"
+            self._send("PING!")
         elif cmd == Command.CLOSE:
             self.close()
-            return
         else:
             msg = f"Unknown command: {cmd}"
             _log.error(msg)
             raise ValueError(msg)
 
-        return self._send(resp)
+    def step(self):
+        if self.__act_space_keys:
+            action = self._recv_dict(self.__act_space_keys)
+        else:
+            action = self._receive()
+        self._send_timestep(lambda: self._env.step(action))
 
-    def __getattr__(self, name):
-        return getattr(self._env, name)
+    def action_space(self):
+        act_space = self._env.action_space
+        if isinstance(act_space, Mapping):
+            self._send_dict(self.__act_space_keys, act_space)
+        else:
+            self._send(act_space)
+
+    def observation_space(self):
+        self._send_dict(self.__obs_space_keys, self._env.observation_space)
+
+    def reset(self):
+        self._send_timestep(lambda: self._env.reset())
+
+    def _send_timestep(self, timestep_fn):
+        # won't work if exception occur not in timestep_fn
+        try:
+            timestep = timestep_fn()
+            self._send_dict(self.__obs_space_keys, timestep.observation)
+            self._send(timestep._replace(observation=None))
+        except Exception as exp:  # env agnostic
+            _log.error(exp)
+            self._send(exp)
+            raise
 
     def close(self):
         self._send(self._env.close())
